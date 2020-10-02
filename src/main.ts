4cd817dd7e3ba29eb40f8a80e2e554e4fdc15601
thiagodantasv <thiagodantasv@gmail.com>
2020-10-02 20:55:18 -0300

    port constant and console.log warning where the app is listening

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const port = 10000;
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(port);
+  console.log(`Listening at port ${port}`);
 }
 bootstrap();
