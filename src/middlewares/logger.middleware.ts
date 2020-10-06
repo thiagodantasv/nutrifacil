7314ba698dd031b9e61eca2fa8d12c6b428f1be9
thiagodantasv <thiagodantasv@gmail.com>
2020-10-06 20:31:07 -0300

    some request possibilities and usefull comments

--- src/middlewares/logger.middleware.ts
+++ src/middlewares/logger.middleware.ts
@@ -5,8 +5,14 @@ import { Request, Response } from 'express';
 export class LoggerMiddleware implements NestMiddleware{
     use(req: Request, res: Response, next: Function){
         console.log('Middleware working ->');
-        // console.log('Request ->', req);
-        // console.log('Response ->', res);
+        //some examples of what can be seen using this req:Request from express paramater;
+        console.log('Request Method ->', req.method);
+        console.log('Request Params ->', req.params);
+        console.log('Request Url ->', req.url);
+        //And there is a lot more that can be seen using this res:Response from express paramater, both properties and methods;
+        // console.log('Response ->', res.json);
+        
+        // the next() is mandatory. If you configured more than one middleware, it will pass to the next one, if you dont, it will just finish the middleware process.
         next();
     }
 }
