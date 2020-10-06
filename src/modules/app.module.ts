af8ddf730c0dbf8d0a312c30c7fcf2eb89037cb4
thiagodantasv <thiagodantasv@gmail.com>
2020-10-05 21:19:27 -0300

    comments about @Module decorator param and the exports param.

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from '../controllers/app.controller';
 import { AppService } from '../services/app.service';
 
 @Module({
-  imports: [],
+  imports: [], // the list of modules that export the providers needed in this module
   controllers: [AppController],
   providers: [AppService],
+  exports:[] // exports added -> they are very important and useful. They define which providers this module provide and need to be available to others modules that import this module
 })
 export class AppModule {
   // what if i want to program something here ? What can i program here ?
